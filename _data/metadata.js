aa86e85c971a3200f24e6d94f0cfe4fac06babfb
Espritdepaix <c.delmas67@gmail.com>
2023-07-20 18:32:36 +0200

    metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Dans Sa douce présence, je trouve la paix",
-	url: "https://espritdepaix.netlify.app",
+	url: "https://espritdepaix.com",
 	language: "fr",
 	description: "Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie. Mise à jour chaque mois avec des audio et des textes. ",
 	author: {
 		name: "Christian Delmas",
 		email: "c.delmas67@gmail.com",
-		url: "https://esprit-de-paix.netlify.app/cotact/"
+		url: "https://espritdepaix.com/contact/"
 	}
 }
