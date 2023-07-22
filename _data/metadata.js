efe6ea3bc7eff150a6d86e6d3677e8b59e03ad7a
Espritdepaix <c.delmas67@gmail.com>
2023-07-22 09:13:09 +0200

    metadata footer index.njk

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "Dans Sa douce présence, je trouve la paix",
+	title: "Esprit de Paix - Dans Sa douce présence, je trouve la paix",
 	url: "https://espritdepaix.com",
 	language: "fr",
 	description: "Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie. Mise à jour chaque mois avec des audio et des textes. ",
