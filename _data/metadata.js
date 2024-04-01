c7e94bbe13dbecbfa7f3d481ebea7033caa86cd6
Espritdepaix <c.delmas67@gmail.com>
2024-04-01 12:14:29 +0200

    metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Conscience de Paix et de Guérison",
-	url: "https://espritdepaix.com",
+	url: "https://conscience-de-paix-et-de-guerison.com/",
 	language: "fr",
-	description: "Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie. Mise à jour chaque mois avec des audio et des textes. ",
+	description: "Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie. Guérison spirituelle à distance ou à domicile. ",
 	author: {
 		name: "Christian Delmas",
 		email: "c.delmas67@gmail.com",
-		url: "https://espritdepaix.com/contact/"
+		url: "https://conscience-de-paix-et-de-guerison.com/contact/"
 	}
 }
