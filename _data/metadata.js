dd6a9c0c394f9ba4e3e538c6c57c86d7c5710527
Espritdepaix <c.delmas67@gmail.com>
2024-04-05 00:09:08 +0200

    metadata + index

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-	title: "Conscience de Paix et de Guérison",
+	title: "Conscience de Paix et de Guérison | Guérison spirituelle",
 	url: "https://conscience-de-paix-et-de-guerison.com",
 	language: "fr",
-	description: "Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie. Guérison spirituelle à distance ou à domicile. ",
+	description: "Guérison spirituelle à distance ou à domicile. Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie.  ",
 	author: {
 		name: "Christian Delmas",
 		email: "c.delmas67@gmail.com",
