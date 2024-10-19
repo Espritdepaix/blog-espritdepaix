909bf2d18a4749eab6b4aff02157d617a31e9495
Espritdepaix <c.delmas67@gmail.com>
2024-10-19 12:03:24 +0200

    data + footer

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-	title: "Guérison spirituelle",
+	title: "Accompagnement et soin spirituel en Alsace",
 	url: "https://conscience-de-paix-et-de-guerison.com",
 	language: "fr",
-	description: "Guérison spirituelle à distance ou à domicile. Faites connaissance avec des textes mystiques de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie.  ",
+	description: "Bénéficiez de soins spirituels à distance ou à domicile. Familiarisez-vous avec les œuvres de Joel S. Goldsmith et d'autres praticiens de la Voie Infinie. ",
 	author: {
 		name: "Christian Delmas",
 		email: "c.delmas67@gmail.com",
